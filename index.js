17beb834e14a4770c415f681841c9048224db796
Joe Andaverde <joe@andaverde.net>
2012-09-30 18:00:34 -0500

    Initial module creation

---
+++ index.js
@@ -0,0 +1,21 @@
+var map = {
+   'a': '4',
+   'e': '3',
+   'f': 'ph'
+   'g': '9',
+   'l': '1',
+   'o': '0',
+   's': '5',
+   't': '7',
+   'y': '`/'
+};
+
+module.exports = function (str) {
+   var newStr = '';
+
+   for (var i = 0; i < str.length; i++) {
+      newStr += map[str[i].toLowerCase() || str[i]];
+   }
+
+   return newStr;
+}
